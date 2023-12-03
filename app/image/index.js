66b61a4b0e88f3e90b6466cc387aafbb46e31964
Arunabh verma <arunabhverma@Arunabhs-MacBook-Air.local>
2023-12-03 21:04:13 +0530

    Related commit(s): added image select logic

---
+++ app/image/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { View, Image, Button, Dimensions } from "react-native";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const index = () => {
+  const [state, setState] = useState({
+    width: 0,
+    height: 0,
+  });
+  const { uri } = useLocalSearchParams();
+  const navigation = useNavigation();
+  const { top } = useSafeAreaInsets();
+
+  Image.getSize(uri, (width, height) => {
+    let ratio = Dimensions.get("screen").width / width;
+    let newHeight = height * ratio;
+    setState((prev) => ({
+      ...prev,
+      width: Dimensions.get("screen").width,
+      height: newHeight,
+    }));
+  });
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          marginTop: top,
+          backgroundColor: "red",
+          position: "absolute",
+          zIndex: 1,
+        }}
+      >
+        <Button title="Back" onPress={() => navigation.goBack()} />
+      </View>
+      <Image
+        key={index.toString()}
+        style={{
+          width: state.width,
+          height: state.height,
+          objectFit: "cover",
+        }}
+        source={{ uri: uri }}
+      />
+    </View>
+  );
+};
+
+export default index;
